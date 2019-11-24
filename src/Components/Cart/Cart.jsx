115434dc7d51eb892a50fb2449e1f190523bcd91
Lewis Nozaki <lhnozaki@gmail.com>
2019-11-23 19:01:39 -1000

    updates

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -5,7 +5,16 @@ class Cart extends Component {
   render() {
     return (
       <div className={styles.container}>
-        <h1>Cart</h1>
+        <div className={styles.cart}>
+          <h1>A Blurange Pineapple</h1>
+          <button>Buy</button>
+        </div>
+        <div className={styles.img}>
+          <img
+            src="https://images.unsplash.com/photo-1494252713559-f26b4bf0b174?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+            alt="#"
+          />
+        </div>
       </div>
     );
   }
